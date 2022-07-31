f68b5982a1d341185613c18f7b61eb2573e2b529
Arakis14 <wiktor.lasota@vp.pl>
2022-07-31 17:45:09 +0200

    strStr finished

--- main.cpp
+++ main.cpp
@@ -221,7 +221,25 @@ int removeDuplicates(std::vector<int>& nums) {
     auto res = nums.size();
     return res;
 }
+
+/*
+Implement strStr().
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, 
+or -1 if needle is not part of haystack.
+Clarification:
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to 
+C's strstr() and Java's indexOf().
+Example 1:
+Input: haystack = "hello", needle = "ll"
+Output: 2
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+*/
+int strStr(const std::string& haystack, const std::string& needle) {
+    return haystack.find(needle, 0);
+}
 int main()
 {
-    
+    std::cout << strStr("aaaa", "bba");
 }
